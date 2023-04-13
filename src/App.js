0ba1d41058aac5510481cb77ef91fa7fec1a3a19
Danny <ddeneau@terpmail.umd.edu>
2023-04-13 18:00:16 -0400

    Went from a barely working web app, to something.
    Fixed the main function JavaScript that sends the requests.
    Added const variables with useState() for the text input.
    Fixed the button.
    Added a results part of the page.
    
    What does the app do now? It sends a request to a Zillow API once the button is pushed.
    
    The results shown on screen are JSON objects with results of property listings in a given area.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
 import './App.css';
-import { Button, TextInput } from 'react-native-web';
+import { Button, TextInput, View } from 'react-native-web';
+import {useState} from 'react'
+import styles from './UI/customComponents'
 
 
 
 function App() {
-  var start_date = '1'
-  var end_date = '1'
+  // Set up the variables needed to change dates and results.
+  const [start_date, set_start_date] = useState('')
+  const [end_date, set_end_date] = useState('')
+  const [results, set_results] = useState('')
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '8b5742ff19msh24018e26d4b87c3p1979fdjsn8ce11750e43f',
-      'X-RapidAPI-Host': 'zillow56.p.rapidapi.com'
-    } 
-  };
+  // Use variables, Zillow API and keys to search for desired information.
+  const handleSearch = () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '8b5742ff19msh24018e26d4b87c3p1979fdjsn8ce11750e43f',
+        'X-RapidAPI-Host': 'zillow56.p.rapidapi.com'
+      }
+    };
 
-  var resp = fetch('https://zillow56.p.rapidapi.com/search?location=houston%2C%20tx', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    fetch(`https://zillow56.p.rapidapi.com/search?location=houston%2C%20tx&`, options)
+      .then(response => response.json())
+      .then(response => set_results(response.results))
+      .catch(err => console.error(err));
 
-   
-  var results = resp['results']
-
-  for (let index in results) {
-    console.log(index['city'])
+      document.getElementById("results").innerText = results
   }
 
+  const handleStartDateChange = (event) => {
+    set_start_date(event.target.value);
+  }
+
+  const handleEndDateChange = (event) => {
+    set_end_date(event.target.value);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-       
-          <h3> Enter Start and End Dates </h3>
-          <TextInput></TextInput>
-          <p>Start</p> {start_date}
-          <span></span>
-          <p>End</p>
-          <TextInput></TextInput>
-          {end_date} 
-          <Button></Button>
-      </header>
-    </div>
+      <div className="controls">
+        <header className="App-header">
+            <h3> Enter Start and End Dates </h3>
+            <p>Start Date</p>
+            <TextInput type="date" style={styles.text_input} value={start_date} onChange={handleStartDateChange}></TextInput>
+            <span></span>
+            <p>End Date</p>
+            <TextInput type="date" style={styles.text_input} value={end_date} onChange={handleEndDateChange}></TextInput>
+            <View style={styles.activate_button}>
+              <Button title='Send Request' onPress={handleSearch} ></Button>
+            </View>
+            <div className="results">
+              <h3> Resuts: </h3>
+              <p id='results'></p>
+            </div>
+        </header>
+      </div>
   );
 }
 
---
+++ src/UI/customComponents.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native-web'
+
+const styles = StyleSheet.create(
+    {text_input: {
+        height: 25, 
+        width: "25%", 
+        backgroundColor: 'grey',
+        borderRadius: 15 }},
+    {activate_button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black',
+    }})
+
+export default styles
